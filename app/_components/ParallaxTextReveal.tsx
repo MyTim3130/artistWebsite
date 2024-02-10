4572a56bd47280897d07d03993d886ecb893fe01
MyTim3130 <tin.hauslkramper@gmail.com>
2024-02-10 12:54:20 +0100

    parallaxTest

---
+++ app/_components/ParallaxTextReveal.tsx
@@ -0,0 +1,69 @@
+'use client'
+import React, { useEffect } from "react";
+import Image from "next/image";
+import gsap from "gsap";
+import {ScrollTrigger} from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
+
+
+const ParallaxTextReveal = () => {
+
+    useEffect(()=>{
+        gsap.fromTo("#imageWrapper1",{y:0},{y:-400, scrollTrigger:{
+            trigger: '#imageWrapper1',
+            start: "top bottom",
+            end: "top top",
+            scrub: true,
+
+        }})
+
+        gsap.fromTo("#imageWrapper2",{y:0},{y:-400, scrollTrigger:{
+            trigger: '#imageWrapper2',
+            start: "top bottom",
+            end: "top top",
+            scrub: true,
+
+        }})
+
+        gsap.fromTo("#imageWrapper3",{y:0},{y:-400, scrollTrigger:{
+            trigger: '#imageWrapper3',
+            start: "top bottom",
+            end: "top top",
+            scrub: true,
+
+        }})
+    },[])
+
+  return (
+    <section className="h-screen w-screen relative">
+      <div className="h-[120vh] w-screen" id="imageWrapper1">
+        <Image
+          src="/images/parallax1.png"
+          objectFit="cover"
+          layout="fill"
+          alt="missingBackground"
+          className="fixed"
+        />
+      </div>
+      <div className="h-[120vh] w-screen relative bottom-10" id="imageWrapper2">
+        <Image
+          src="/images/parallax2.png"
+          objectFit="cover"
+          layout="fill"
+          alt="missingBackground"
+        />
+      </div>
+      <div className="h-[120vh] w-screen relative bottom-16" id="imageWrapper3">
+        <Image
+          src="/images/parallax3.png"
+          objectFit="cover"
+          layout="fill"
+          alt="missingBackground"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default ParallaxTextReveal;
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import LandingPage from "./_components/LandingPage";
 import About from "./_components/About";
 import About1 from "./_components/About1";
+import ParallaxTextReveal from "./_components/ParallaxTextReveal";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
     <LandingPage />
     {/* <About /> */}
     <About1 />
+    <ParallaxTextReveal />
    </>
   );
 }
